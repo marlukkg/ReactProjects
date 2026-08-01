aac4bee17df72650fed685a2d7972a3ae30234f3
Marko Lukic <marlukkg@gmail.com>
2026-08-01 17:17:34 +0200

    Add a back to home link

--- react-hello-world/src/NotFoundPage.jsx
+++ react-hello-world/src/NotFoundPage.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 function NotFoundPage() {
 
   return (
     <div>
       <h1 style={{ paddingTop: '150px' }}>404 Not Found</h1>
+      <Link to="/">Home</Link>
     </div>
   )
 }
