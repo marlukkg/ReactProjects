543cfbd3f4a2e3f6f488186426f9fd08dad620e2
Marko Lukic <marlukkg@gmail.com>
2026-08-01 17:14:41 +0200

    Add a Not Found Page

---
+++ react-hello-world/src/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+
+  return (
+    <div>
+      <h1 style={{ paddingTop: '150px' }}>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
--- react-hello-world/src/main.jsx
+++ react-hello-world/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import NotFoundPage from './NotFoundPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <NotFoundPage />
   }
 ]);
 
