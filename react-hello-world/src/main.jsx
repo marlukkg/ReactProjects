94be3e7374a6e2c342901b3e453d5a69dfc64acc
Marko Lukic <marlukkg@gmail.com>
2026-08-08 13:57:40 +0200

    Move to browserRouter and add navigation

--- react-hello-world/src/main.jsx
+++ react-hello-world/src/main.jsx
@@ -1,25 +1,31 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Link, BrowserRouter, Routes, Route, NavLink, Outlet, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import NotFoundPage from './NotFoundPage.jsx'
 import About from './About.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFoundPage />
-  },
-  {
-    path: '/about',
-    element: <About />
-  }
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <div>
+        {/* Navigation Bar */}
+        <nav style={{ padding: '10px', backgroundColor: '#0c1287', display: 'flex', gap: '20px',  }}>
+          <Link to="/" >Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+
+        {/* Route Configuration */}
+        <main style={{ padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   </StrictMode>,
 )
