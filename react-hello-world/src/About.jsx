fd14a8d1f1c103408fe6557c057d0bdc660628c4
Marko Lukic <marlukkg@gmail.com>
2026-08-26 19:43:56 +0200

    Adding simple info to the about page

--- react-hello-world/src/About.jsx
+++ react-hello-world/src/About.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 function About() {
 
   return (
     <div>
-      <h1 style={{ paddingTop: '150px' }}>About</h1>
+      <h1 style={{ paddingTop: '80px' }}>About</h1>
+      <p>Welcome to my website.<br/><br/>
+      My name is Marko, and I have been a Web Developer for 7 years.<br/>
+      If you would be interested in hiring me, please take a look at my <Link to="https://www.linkedin.com/in/marlukweb/">LinkedIn</Link> profile for more information about my experience.</p>
     </div>
   )
 }
